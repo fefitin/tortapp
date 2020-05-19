5e2c43dce228023a84021e762e66455799b5d7c4
Federico García <fede@divinodi.com>
2020-05-19 20:27:27 -0300

    Almacenar receta en localStorage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 import Header from './components/Header';
 import Ingredients from './components/Ingredients';
 import Pan from './components/Pan';
+import Ingredient from './classes/Ingredient';
 
 library.add(faTrashAlt);
 
@@ -12,9 +13,9 @@ function App() {
   const [ ingredients, setIngredients ] = useState([]);
   const [ ratio, setRatio ] = useState(1);
 
-  const addIngredient = text => {
-    setIngredients(ingredients.concat(text));
-  }
+  const addIngredient = useCallback(ingredient => {
+    setIngredients(ingredients => ingredients.concat(ingredient));
+  }, []);
 
   const deleteIngredient = index => {
     setIngredients(ingredients.filter((value, i) => index !== i));
@@ -23,6 +24,24 @@ function App() {
   const onRatioUpdate = ratio => {
     setRatio(ratio);
   }
+
+  useEffect(() => {
+    //Load recipe from localStorage
+    if(localStorage.ingredients) {
+      try {
+        JSON.parse(localStorage.ingredients).forEach(text => {
+          addIngredient(new Ingredient(text));
+        })
+      } catch(error) {
+        delete localStorage.ingredients;
+      }
+    }
+  }, [addIngredient])
+
+  useEffect(() => {
+    //Save recipe to localStorage
+    localStorage.ingredients = JSON.stringify(ingredients.map(i => i.text));
+  }, [ingredients])
 
   return (
     <div className="App">
